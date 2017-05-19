968fa3163a36858434732913700d021181c21356
VictorMora <victor.mora.benambourg@gmail.com>
2017-05-19 15:39:33 -0600

    Initial commit

---
+++ app/src/main/java/com/example/thor0/intentsexample/Contacto.java
@@ -0,0 +1,41 @@
+package com.example.thor0.intentsexample;
+
+/**
+ * Created by thor0 on 01-May-17.
+ */
+
+public class Contacto {
+
+    private String nombre;
+    private String telefono;
+    private String email;
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
